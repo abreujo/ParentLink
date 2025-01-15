34d3ad14f281cea92fd64d0a2df1f5d6ca86a928
Johel <johel.abreu@gmail.com>
2025-01-15 23:55:17 +0100

    UserSystemDto implementacion

--- enpoints/enpoints/src/main/java/com/parentlink/dto/UserSystemDto.java
+++ enpoints/enpoints/src/main/java/com/parentlink/dto/UserSystemDto.java
@@ -1,4 +1,19 @@
 package com.parentlink.dto;
 
 public class UserSystemDto {
+    private String username;
+
+    // Constructor
+    public UserSystemDto(String username) {
+        this.username = username;
+    }
+
+    // Getters y Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
