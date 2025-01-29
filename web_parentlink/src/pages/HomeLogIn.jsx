90ba5cbe3f43683628b42d278cd39d8df388a540
Myroslav-Petro Palko <mirpalko@gmail.com>
2025-01-29 08:57:22 +0100

    diseno

--- web_parentlink/src/pages/HomeLogIn.jsx
+++ web_parentlink/src/pages/HomeLogIn.jsx
@@ -5,7 +5,7 @@ import EventSection from "../components/EventSection";
 import WelcomeMessage from "../components/WelcomeMessage";
 import { useAuth } from "../contex/AuthContext";
 import Menu from "../components/Menu";
-
+import "../styles/HomeLogIn.css";
 const HomeLogIn = () => {
   const { username } = useAuth();
 
